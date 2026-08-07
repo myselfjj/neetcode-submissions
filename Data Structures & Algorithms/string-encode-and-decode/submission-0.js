0c5cf82dbc25ee87c60567550508618a4df6deb2
Jayesh Jadhav <43574791+myselfjj@users.noreply.github.com>
2026-08-07 12:54:29 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+
+        let str = strs.map( val => `${val.length}#${val}`).join("")
+
+        return str
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+
+        let ans=[]
+
+        let i=0
+        
+        while(i<str.length){
+
+            let j=i;
+
+            while(str[j]!='#'){
+                j++
+            }
+
+            let len= parseInt(str.slice(i, i+j+1))
+
+            ans.push(str.slice(j+1, j+1+len))
+
+            i=j+1+len
+        }
+
+        return ans
+    }
+}
